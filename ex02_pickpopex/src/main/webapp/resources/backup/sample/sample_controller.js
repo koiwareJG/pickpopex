d848218433692d3aca918783584efd06b2d5f4e2
koiwareJG <koiware.jg@gmail.com>
2016-01-25 20:56:29 +0900

    샘플추가

---
+++ ex02_pickpopex/src/main/webapp/resources/backup/sample/sample_controller.js
@@ -0,0 +1,31 @@
+/* ==============================================================================
+ * 파일명 : 파일명.js
+ * 화면명 : 메뉴명 > 상품 목록 (Contoller)
+ * 작성자 : 홍길동
+ * 작성일 : 2016.01.25
+ * ==============================================================================
+ */ 
+
+'use strict';
+
+define([], function () {
+	function _controller($scope, tester) {
+		console.log("sample_list contorller loaded"); //파일명과 동일하게 맞출것
+		
+		//CSS 로딩 설정 S
+		$scope.$emit('updateCSS', ['resources/css/partials-css/sample/sample.css']);//로드할 css파일 경로 설정(없으면 삭제)
+		//CSS 로딩 설정 E
+		
+		//데이터 세팅 S (받아올 데이터 없으면 삭제)
+		$http.get("http://localhost:8090/controller/sample/sendList").then(function(response) {
+		        $scope.myData = response.data.records;
+		});
+		//데이터 세팅 E
+		
+		//화면 별 기능 구현 S
+
+		
+		//화면 별 기능 구현 E
+	}
+	return _controller;
+});
