f682dc187c4d200d61fa9a1a1d9ad796ec52792d
koiwareAJH <koiwareAJH@koiware-PC>
2016-02-04 17:29:53 +0900

    pull 받기전 합침

---
+++ ex02_pickpopex/src/main/webapp/resources/js/controllers/checkout/checkout_purchase.js
@@ -0,0 +1,20 @@
+/* ==============================================================================
+ * 파일명 : 파일명.js
+ * 화면명 : 메뉴명 > 상품 목록 (Contoller)
+ * 작성자 : 홍길동
+ * 작성일 : 2016.01.25
+ * ==============================================================================
+ */ 
+
+'use strict';
+
+define([], function () {
+	function _controller($scope) {
+		console.log("checkout_purchase contorller loaded"); //파일명과 동일하게 맞출것
+		
+		//CSS 로딩 설정 S
+		$scope.$emit('updateCSS', ['resources/css/partials-css/checkout/checkout_purchase.css', 'resources/css/partials-css/checkout/checkout_common.css']);//로드할 css파일 경로 설정(없으면 삭제)
+		//CSS 로딩 설정 E
+	}
+	return _controller;
+});
